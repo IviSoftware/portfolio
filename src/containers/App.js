9f2dcfbd719442d445deed278bb844e90de2b5d6
IviSoftware <iva_men_ase@hotmail.com>
2022-09-10 23:45:21 -0500

    responsive aún no terminado

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,16 +2,18 @@ import Navbar from '../components/Navbar';
 import About from './About';
 import Header from './Header';
 import Projects from './Projects';
+import SocialNetwork from './SocialNetwork';
+
 
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
-     <Header />
-     <Projects />
-     <About />
-    
+      <Navbar />
+      <Header />
+      <Projects />
+      <About />
+      <SocialNetwork />    
     </div>  
   );
 }
---
+++ src/containers/SocialNetwork.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function SocialNetwork() {
+  return (
+    <section className="SocialNetwork">
+      
+    </section>
+  )
+}
+
+export default SocialNetwork
